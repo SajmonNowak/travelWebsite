f810719168341a3fee71b8d3f94ac862eb69a103
sajmon <nowak.sajmon@gmail.com>
2021-10-02 11:52:02 +0200

    Added Trips section and GraphiQL hook

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,31 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "travelWebsite",
   },
-  plugins: ["gatsby-plugin-styled-components",
-  `gatsby-transformer-json`,
+  plugins: [
+    "gatsby-plugin-styled-components",
+    `gatsby-transformer-json`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `./src/data/`,
       },
-    },],
-  
-
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `video`,
+        path: `${__dirname}/src/assets/videos`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+  ],
 };
--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,13 +1,55 @@
-import React from 'react'
-import { ProductsContainer, ProductsHeading, ProductsWrapper } from './styles/Trips.styled'
+import React from "react";
+import {
+  ProductsContainer,
+  ProductsHeading,
+  ProductsWrapper,
+} from "./styles/Trips.styled";
+import { useStaticQuery, graphql } from "gatsby";
+import  { GatsbyImage, getImage} from "gatsby-plugin-image"
 
 const Trips = () => {
-    return (
-        <ProductsContainer>
-            <ProductsHeading></ProductsHeading>
-            <ProductsWrapper></ProductsWrapper>
-        </ProductsContainer>
-    )
-}
+  const data = useStaticQuery(graphql`
+    query TripsQuery {
+      allTripsJson {
+        edges {
+          node {
+            alt
+            button
+            name
+            img {
+              childImageSharp {
+                gatsbyImageData(layout: CONSTRAINED)
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
 
-export default Trips
+  function getTrips(data) {
+      const tripsArray = [];
+
+      data.allTripsJson.edges.forEach((trip, index) => {
+          let tripImage= getImage(trip.node.img.childImageSharp.gatsbyImageData)
+          tripsArray.push(
+              <div key={index}>
+                  <GatsbyImage image={tripImage}
+                  alt={trip.node.alt}
+                  />
+
+              </div>
+          )
+      })
+      return tripsArray;
+  }
+
+  return (
+    <ProductsContainer>
+      <ProductsHeading></ProductsHeading>
+      <ProductsWrapper>{getTrips(data)}</ProductsWrapper>
+    </ProductsContainer>
+  );
+};
+
+export default Trips;
